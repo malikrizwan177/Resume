4c67085c4be244a0e8fa0af9a7625e79c8ac689c
malikrizwan177 <malik.rizwan177@yahoo.com>
2024-04-01 22:20:34 +0500

    home page overflow fixed

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -6,7 +6,7 @@ import './Home.css'
 const Home = () => {
 
   return (
-    <>
+    <div className='topContainer'>
     <div className='mainContainer animate__animated animate__fadeInUp'>
     <h1 className='mainHeading'>Welcome to my Portfolio website</h1>
     <h2 className='subHeading'>It's a basic React website with the AntDesign library used in it for an active Resume Portfolio. It's part of the post-internship projects aimed to 
@@ -17,7 +17,7 @@ const Home = () => {
       <Link key="logo" className='descriptionButton animate__animated animate__fadeInLeft animate__delay-1s' to={`/about`}><Button style={{width:'100%'}} ghost size='large'>Explore More</Button></Link>
       <Link key="siteInfo" className='descriptionButton animate__animated animate__fadeInRight animate__delay-1s' to={`/siteInfo`}><Button style={{width:'100%'}} ghost size='large' >Website Info</Button></Link>
     </div> 
-    </>
+    </div>
   );
 };
 
